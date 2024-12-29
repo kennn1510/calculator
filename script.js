abe20278cde738ce77dfb665e08c94532c1b0559
Ken Nguyen <46609017+kennn1510@users.noreply.github.com>
2024-12-29 11:06:52 +0700

    Add, subtract, multiply, divide, operate, a, operator, b

---
+++ script.js
@@ -0,0 +1,28 @@
+let a;
+let operator;
+let b;
+
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+function operate(a, operator, b) {
+  if (operator === "+") return add(a, b);
+  if (operator === "-") return subtract(a, b);
+  if (operator === "*") return multiply(a, b);
+  if (operator === "/") return divide(a, b);
+}
+
+console.log(operate(5, "*", 5));
