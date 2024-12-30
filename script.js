f2528d58c2f8f13eb95a6dccfb7270477706b82d
Ken Nguyen <46609017+kennn1510@users.noreply.github.com>
2024-12-30 16:55:55 +0700

    Trying to get b to work

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-let a;
-let operator;
-let b;
+let a = 0;
+let operator = undefined;
+let b = 0;
 const span = document.querySelector("span.display-right-padding");
 const equal = document.querySelector(".equal");
 const numbers = document.querySelectorAll(".num");
@@ -9,15 +9,45 @@ const buttons = document.querySelectorAll("button");
 buttons.forEach((button) => {
   button.addEventListener("click", (e) => {
     if (button.classList.contains("num")) {
-      span.innerText = e.target.innerText;
-      a = parseInt(e.target.innerText);
-      console.log(a);
-    } else {
-      operator = e.target.innerText;
-      console.log(operator);
+      if (operator === undefined) {
+        if (span.innerText === "0") {
+          span.innerText = button.innerText;
+          a = parseInt(span.innerText);
+        } else {
+          span.innerText += button.innerText;
+          a = parseInt(span.innerText);
+        }
+      } else {
+        // span.innerText = "0";
+        if (span.innerText === "0") {
+          span.innerText = button.innerText;
+          b = parseInt(span.innerText);
+        } else {
+          span.innerText += button.innerText;
+          b = parseInt(span.innerText);
+        }
+      }
+      console.log(b);
+    }
+    if (button.classList.contains("operand")) {
+      operator = button.innerText;
+    }
+    if (button.classList.contains("equal")) {
+      span.innerText = operate(a, operator, b);
+      a = parseInt(span.innerText);
+    }
+    if (button.classList.contains("clear")) {
+      clear();
     }
   });
 });
+
+function clear() {
+  a = 0;
+  b = 0;
+  operator = undefined;
+  span.innerText = "0";
+}
 
 function add(a, b) {
   return a + b;
